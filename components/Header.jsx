ac8bf95cfe63814013576435be5efe4f885f898c
Max de Vocht <maxdevocht@hotmail.com>
2024-03-21 11:56:35 +0100

    Update
    Created Header component

---
+++ components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <header>
+            <div className="title">
+                <img src="./src/assets/Troll-Face.svg" alt="Logo" />
+                <h1>Meme Generator</h1>
+            </div>
+            <h2>A Dynamic React App</h2>
+        </header>
+    )
+}
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import Header from '../components/Header'
+import './App.css'
+
+export default function App() {
+  return (
+    <Header />
+  )
+}
+
