84941416486d0e0940243d336e3e5b826d81acb8
Max de Vocht <maxdevocht@hotmail.com>
2024-03-21 12:29:33 +0100

    Adjusted form styling

--- components/Meme.jsx
+++ components/Meme.jsx
@@ -5,17 +5,23 @@ export default function Meme() {
         <main>
             <form className='form' action="">
 
-                <input 
-                    className='form--input' 
-                    type="text" 
-                    placeholder='Top text'
-                />
+                <label htmlFor='top-text' className='form--label'>Top Text
+                    <input 
+                        id='top-text'
+                        className='form--input' 
+                        type="text" 
+                        placeholder='Shut up'
+                    />
+                </label>
 
-                <input 
-                    className='form--input' 
-                    type="text" 
-                    placeholder='Bottom text'
-                />
+                <label htmlFor='bottom-text' className='form--label'>Bottom text
+                    <input 
+                        id='bottom-text'
+                        className='form--input' 
+                        type="text" 
+                        placeholder='and take my money'
+                    />
+                </label>
 
                 <button className='form--btn'>Get a new meme image  🖼</button>
             </form>
