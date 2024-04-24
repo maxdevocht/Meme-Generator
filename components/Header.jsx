987d5e063088ebf7fc886258ad6ddbd10b73de85
Max de Vocht <maxdevocht@hotmail.com>
2024-04-24 11:25:48 +0200

    Finish

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <header>
             <div className="title">
-                <img className='header--img' src="./src/assets/Troll-Face.svg" alt="Logo" />
+                <img className='header--img' src="/Troll-Face.svg" alt="Logo" />
                 <h1>Meme Generator</h1>
             </div>
             <h2>A Dynamic React App</h2>
